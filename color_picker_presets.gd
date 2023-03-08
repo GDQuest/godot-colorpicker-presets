2789b902c616bec91168807d46250fdf177bc7ca
Răzvan C. Rădulescu <razcore.rad@gmail.com>
2023-03-08 11:59:55 +0200

    chore: remove forgotten print call

--- color_picker_presets.gd
+++ color_picker_presets.gd
@@ -1,12 +1,12 @@
 @tool
 extends EditorPlugin
 
+
 const PRESETS_FILENAME := 'presets.hex'
 
 
 func _enter_tree() -> void:
 	var presets_path: String = get_script().resource_path.get_base_dir().path_join(PRESETS_FILENAME)
-	print(presets_path)
 	var presets_file := FileAccess.open(presets_path, FileAccess.READ)
 
 	if FileAccess.get_open_error() == OK:
